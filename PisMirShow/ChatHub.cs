3ed9db19fc5c1d750ef062e7a20fab57c502349a
Dinamokid dinamokidov <dinamokid@yandex.ru>
2019-03-31 17:38:09 +0700

    Чат с обозначением имен

--- PisMirShow/ChatHub.cs
+++ PisMirShow/ChatHub.cs
@@ -5,9 +5,9 @@ namespace SignalRApp
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message)
+        public async Task Send(string message, string userName)
         {
-            await this.Clients.All.SendAsync("Send", message);
+            await this.Clients.All.SendAsync("Send", message, userName);
         }
     }
 }
